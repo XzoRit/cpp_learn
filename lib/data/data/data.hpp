1943345215abffd6ad53e25ed3bdbf249f6563b1
XzoRit <xzorit@hotmail.com>
2022-10-14 18:41:13 +0200

    data type shall be serializeable

--- lib/data/data/data.hpp
+++ lib/data/data/data.hpp
@@ -1,5 +1,74 @@
 #pragma once
 
+#include <ostream>
+#include <vector>
+
+namespace xzr::learn::data
+{
+inline namespace v1
+{
+template <class A>
+struct container : public std::vector<A>
+{
+  public:
+    using std::vector<A>::vector;
+
+    const std::vector<A>& base() const
+    {
+        return *this;
+    }
+    std::vector<A>& base()
+    {
+        return *this;
+    }
+};
+template <class A>
+inline std::ostream& operator<<(std::ostream& o, const container<A>& v)
+{
+    o << "{ ";
+    for (auto&& it : v)
+        o << it << ' ';
+    o << '}';
+
+    return o;
+}
+}
+}
+
+#include <boost/pfr/functions_for.hpp>
+
+#define XZR_LEARN_DATA_OPS_FOR(T)                                              \
+    BOOST_PFR_MAYBE_UNUSED inline bool operator==(const T& lhs, const T& rhs)  \
+    {                                                                          \
+        return ::boost::pfr::eq_fields(lhs, rhs);                              \
+    }                                                                          \
+    BOOST_PFR_MAYBE_UNUSED inline bool operator!=(const T& lhs, const T& rhs)  \
+    {                                                                          \
+        return ::boost::pfr::ne_fields(lhs, rhs);                              \
+    }                                                                          \
+    BOOST_PFR_MAYBE_UNUSED inline bool operator<(const T& lhs, const T& rhs)   \
+    {                                                                          \
+        return ::boost::pfr::lt_fields(lhs, rhs);                              \
+    }                                                                          \
+    BOOST_PFR_MAYBE_UNUSED inline bool operator>(const T& lhs, const T& rhs)   \
+    {                                                                          \
+        return ::boost::pfr::gt_fields(lhs, rhs);                              \
+    }                                                                          \
+    BOOST_PFR_MAYBE_UNUSED inline bool operator<=(const T& lhs, const T& rhs)  \
+    {                                                                          \
+        return ::boost::pfr::le_fields(lhs, rhs);                              \
+    }                                                                          \
+    BOOST_PFR_MAYBE_UNUSED inline bool operator>=(const T& lhs, const T& rhs)  \
+    {                                                                          \
+        return ::boost::pfr::ge_fields(lhs, rhs);                              \
+    }                                                                          \
+    template <class Char, class Traits>                                        \
+    BOOST_PFR_MAYBE_UNUSED inline ::std::basic_ostream<Char, Traits>&          \
+    operator<<(::std::basic_ostream<Char, Traits>& out, const T& value)        \
+    {                                                                          \
+        return out << ::boost::pfr::io_fields(value);                          \
+    }
+
 #include <string>
 #include <vector>
 
@@ -12,15 +81,61 @@ struct card
     std::string front{};
     std::string back{};
 };
+
+XZR_LEARN_DATA_OPS_FOR(card)
+
 struct cards
 {
     std::string name{};
-    std::vector<card> content{};
+    container<card> content{};
 };
+
+XZR_LEARN_DATA_OPS_FOR(cards)
+
 struct package
 {
     std::string name{};
-    std::vector<cards> content{};
+    container<cards> content{};
 };
+
+XZR_LEARN_DATA_OPS_FOR(package)
+}
+}
+
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+
+namespace boost::serialization
+{
+template <class Ar, class A>
+inline auto serialize(Ar& a,
+                      ::xzr::learn::data::v1::container<A>& c,
+                      const unsigned int) -> void
+{
+    a& c.base();
+}
+template <class Ar>
+inline auto serialize(Ar& a,
+                      ::xzr::learn::data::v1::card& c,
+                      const unsigned int) -> void
+{
+    a& c.front;
+    a& c.back;
+}
+template <class Ar>
+inline auto serialize(Ar& a,
+                      ::xzr::learn::data::v1::cards& cs,
+                      const unsigned int) -> void
+{
+    a& cs.name;
+    a& cs.content;
+}
+template <class Ar>
+inline auto serialize(Ar& a,
+                      ::xzr::learn::data::v1::package& p,
+                      const unsigned int) -> void
+{
+    a& p.name;
+    a& p.content;
 }
 }
--- lib/data/data/data.test.cpp
+++ lib/data/data/data.test.cpp
@@ -1,57 +1,131 @@
 #include <data/data.hpp>
 
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include <sstream>
 
 namespace
 {
 BOOST_AUTO_TEST_SUITE(data_tests)
+
 BOOST_AUTO_TEST_SUITE(card_tests)
+
 using card = ::xzr::learn::data::card;
+
 BOOST_AUTO_TEST_CASE(card_default_ctor)
 {
-    card a{};
+    const card a{};
     BOOST_TEST(a.front == "");
     BOOST_TEST(a.back == "");
 }
 BOOST_AUTO_TEST_CASE(card_initialized_ctor)
 {
-    card a{.front = "Hello", .back = "Hallo"};
+    const card a{.front = "Hello", .back = "Hallo"};
     BOOST_TEST(a.front == "Hello");
     BOOST_TEST(a.back == "Hallo");
 }
+BOOST_AUTO_TEST_CASE(card_serialization)
+{
+    const card a{.front = "Hello", .back = "Hallo"};
+
+    std::stringstream ss{};
+    {
+        boost::archive::text_oarchive oa{ss};
+        oa << a;
+    }
+
+    card b{};
+    {
+        boost::archive::text_iarchive ia{ss};
+        ia >> b;
+    }
+
+    BOOST_TEST(a == b);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE(cards_tests)
+
 using cards = ::xzr::learn::data::cards;
-BOOST_AUTO_TEST_CASE(cards_ctor)
+
+BOOST_AUTO_TEST_CASE(cards_default_ctor)
 {
     cards a{};
     BOOST_TEST(a.content.empty());
     BOOST_TEST(a.name == "");
 }
-BOOST_AUTO_TEST_CASE(add_cards)
+BOOST_AUTO_TEST_CASE(cards_initialized_ctor)
 {
-    cards a{.name = "ch_01"};
-    a.content.push_back({.front = "Hello", .back = "Hallo"});
+    cards a{.name = "ch_01", .content = {{.front = "Hello", .back = "Hallo"}}};
     BOOST_TEST(!a.content.empty());
     BOOST_TEST(a.content.size() == 1u);
 }
+BOOST_AUTO_TEST_CASE(cards_serialization)
+{
+    const cards a{.name = "ch_01",
+                  .content = {{.front = "Hello", .back = "Hallo"}}};
+
+    std::stringstream ss{};
+    {
+        boost::archive::text_oarchive oa{ss};
+        oa << a;
+    }
+
+    cards b{};
+    {
+        boost::archive::text_iarchive ia{ss};
+        ia >> b;
+    }
+
+    BOOST_TEST(a == b);
+}
 BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE(package_tests)
+
 using package = ::xzr::learn::data::package;
+
 BOOST_AUTO_TEST_CASE(package_ctor)
 {
-    package a{};
+    const package a{};
     BOOST_TEST(a.content.empty());
     BOOST_TEST(a.name == "");
 }
-BOOST_AUTO_TEST_CASE(add_package)
+BOOST_AUTO_TEST_CASE(package_initialized_ctor)
 {
-    package a{.name = "pack_01"};
-    a.content.push_back(
-        {.name = "ch_01", .content = {{.front = "Hello", .back = "Hallo"}}});
+    const package a{
+        .name = "pack_01",
+        .content = {{.name = "ch_01",
+                     .content = {{.front = "Hello", .back = "Hallo"}}}}};
     BOOST_TEST(!a.content.empty());
     BOOST_TEST(a.content.size() == 1u);
 }
+BOOST_AUTO_TEST_CASE(package_serialization)
+{
+    const package a{
+        .name = "pack_01",
+        .content = {{.name = "ch_01",
+                     .content = {{.front = "Hello", .back = "Hallo"}}}}};
+
+    std::stringstream ss{};
+    {
+        boost::archive::text_oarchive oa{ss};
+        oa << a;
+    }
+
+    package b{};
+    {
+        boost::archive::text_iarchive ia{ss};
+        ia >> b;
+    }
+
+    BOOST_TEST(a == b);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
 }
