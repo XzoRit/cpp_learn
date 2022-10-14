2a318dc490395688db01b39c512fe60a4f362654
XzoRit <xzorit@hotmail.com>
2022-10-14 20:35:54 +0200

    remove code duplication for serialization

--- lib/data/data/data.hpp
+++ lib/data/data/data.hpp
@@ -34,42 +34,55 @@ inline auto operator<<(std::ostream& o, const container<A>& v) -> std::ostream&
 }
 }
 
+#include <boost/serialization/vector.hpp>
+
+namespace boost::serialization
+{
+template <class Ar, class A>
+inline auto serialize(Ar& ar,
+                      ::xzr::learn::data::v1::container<A>& a,
+                      const unsigned int) -> void
+{
+    ar& a.base();
+}
+}
+
 #include <boost/pfr/functions_for.hpp>
 
-#define XZR_LEARN_DATA_OPS_FOR(T)                                              \
-    BOOST_PFR_MAYBE_UNUSED inline bool operator==(const T& lhs, const T& rhs)  \
+#include <ostream>
+
+#define XZR_LEARN_DATA_OPS_FOR(A)                                              \
+    [[nodiscard]] inline bool operator==(const A& lhs, const A& rhs)           \
     {                                                                          \
         return ::boost::pfr::eq_fields(lhs, rhs);                              \
     }                                                                          \
-    BOOST_PFR_MAYBE_UNUSED inline bool operator!=(const T& lhs, const T& rhs)  \
+    [[nodiscard]] inline bool operator!=(const A& lhs, const A& rhs)           \
     {                                                                          \
         return ::boost::pfr::ne_fields(lhs, rhs);                              \
     }                                                                          \
-    BOOST_PFR_MAYBE_UNUSED inline bool operator<(const T& lhs, const T& rhs)   \
+    [[nodiscard]] inline bool operator<(const A& lhs, const A& rhs)            \
     {                                                                          \
         return ::boost::pfr::lt_fields(lhs, rhs);                              \
     }                                                                          \
-    BOOST_PFR_MAYBE_UNUSED inline bool operator>(const T& lhs, const T& rhs)   \
+    [[nodiscard]] inline bool operator>(const A& lhs, const A& rhs)            \
     {                                                                          \
         return ::boost::pfr::gt_fields(lhs, rhs);                              \
     }                                                                          \
-    BOOST_PFR_MAYBE_UNUSED inline bool operator<=(const T& lhs, const T& rhs)  \
+    [[nodiscard]] inline bool operator<=(const A& lhs, const A& rhs)           \
     {                                                                          \
         return ::boost::pfr::le_fields(lhs, rhs);                              \
     }                                                                          \
-    BOOST_PFR_MAYBE_UNUSED inline bool operator>=(const T& lhs, const T& rhs)  \
+    [[nodiscard]] inline bool operator>=(const A& lhs, const A& rhs)           \
     {                                                                          \
         return ::boost::pfr::ge_fields(lhs, rhs);                              \
     }                                                                          \
-    template <class Char, class Traits>                                        \
-    BOOST_PFR_MAYBE_UNUSED inline ::std::basic_ostream<Char, Traits>&          \
-    operator<<(::std::basic_ostream<Char, Traits>& out, const T& value)        \
+    [[maybe_unused]] inline ::std::ostream& operator<<(::std::ostream& out,    \
+                                                       const A& value)         \
     {                                                                          \
         return out << ::boost::pfr::io_fields(value);                          \
     }
 
 #include <string>
-#include <vector>
 
 namespace xzr::learn::data
 {
@@ -101,40 +114,20 @@ XZR_LEARN_DATA_OPS_FOR(package)
 }
 }
 
-#include <boost/serialization/string.hpp>
-#include <boost/serialization/vector.hpp>
+#include <boost/pfr/core.hpp>
 
-namespace boost::serialization
-{
-template <class Ar, class A>
-inline auto serialize(Ar& a,
-                      ::xzr::learn::data::v1::container<A>& c,
-                      const unsigned int) -> void
-{
-    a& c.base();
-}
-template <class Ar>
-inline auto serialize(Ar& a,
-                      ::xzr::learn::data::v1::card& c,
-                      const unsigned int) -> void
-{
-    a& c.front;
-    a& c.back;
-}
-template <class Ar>
-inline auto serialize(Ar& a,
-                      ::xzr::learn::data::v1::cards& cs,
-                      const unsigned int) -> void
-{
-    a& cs.name;
-    a& cs.content;
-}
-template <class Ar>
-inline auto serialize(Ar& a,
-                      ::xzr::learn::data::v1::package& p,
-                      const unsigned int) -> void
-{
-    a& p.name;
-    a& p.content;
-}
-}
+#define XZR_LEARN_DATA_SERIALIZE_FOR(A)                                        \
+    namespace boost::serialization                                             \
+    {                                                                          \
+    template <class Ar>                                                        \
+    inline auto serialize(Ar& ar, A& a, const unsigned int) -> void            \
+    {                                                                          \
+        ::boost::pfr::for_each_field(a, [&ar](auto& field) { ar& field; });    \
+    }                                                                          \
+    }
+
+#include <boost/serialization/string.hpp>
+
+XZR_LEARN_DATA_SERIALIZE_FOR(::xzr::learn::data::card)
+XZR_LEARN_DATA_SERIALIZE_FOR(::xzr::learn::data::cards)
+XZR_LEARN_DATA_SERIALIZE_FOR(::xzr::learn::data::package)
