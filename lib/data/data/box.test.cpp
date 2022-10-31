94d539ea8e70796aeef2ba6e8a6686dfbf63edcb
XzoRit <xzorit@hotmail.com>
2022-10-31 09:52:52 +0100

    test when always incorrect answers are given

--- lib/data/data/box.test.cpp
+++ lib/data/data/box.test.cpp
@@ -22,10 +22,10 @@ using chapter = ::xzr::learn::data::chapter;
 auto create_cards()
 {
     auto crds{cards{}};
-    for (int i{}; i < 10;)
+    for (int i{}; i < 10; ++i)
     {
         crds.push_back(
-            {.front = std::to_string(++i), .back = std::to_string(++i)});
+            {.front = std::to_string(i), .back = std::to_string(i + 1)});
     }
 
     return crds;
@@ -35,17 +35,34 @@ BOOST_AUTO_TEST_CASE(all_cards_always_correct)
 {
     auto crds{create_cards()};
     auto b{box{crds}};
-    BOOST_TEST(b.has_next());
+    BOOST_REQUIRE(b.has_next());
 
     for (std::size_t i{}; i < crds.size(); ++i)
     {
         const auto& crd{b.next()};
         const auto it{std::ranges::find(crds, crd)};
-        BOOST_TEST(*it == crd);
+        // correct answer
         b.move_card(crd, it->back);
     }
 
     BOOST_TEST(!b.has_next());
+}
+
+BOOST_AUTO_TEST_CASE(incorrect_cards_are_represented)
+{
+    auto crds{create_cards()};
+    auto b{box{crds}};
+    BOOST_REQUIRE(b.has_next());
+
+    for (std::size_t i{}; i < crds.size(); ++i)
+    {
+        const auto& crd{b.next()};
+        const auto it{std::ranges::find(crds, crd)};
+        // incorrect answer
+        b.move_card(crd, it->front);
+    }
+
+    BOOST_TEST(b.has_next());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
