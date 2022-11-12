7f1dedb5a338ae5cf53348c4ca1c14bd52c63035
XzoRit <xzorit@hotmail.com>
2022-11-12 16:18:26 +0100

    move serialization tests from data tests to serialization tests

---
+++ lib/data/data/serialize.test.cpp
@@ -0,0 +1,74 @@
+#include <data/data.hpp>
+#include <data/serialize.hpp>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/test/unit_test.hpp>
+
+namespace
+{
+BOOST_AUTO_TEST_SUITE(data_tests)
+
+BOOST_AUTO_TEST_SUITE(serialize_tests)
+
+using chapter = ::xzr::learn::data::chapter;
+using card = ::xzr::learn::data::card;
+using book = ::xzr::learn::data::book;
+
+BOOST_AUTO_TEST_CASE(card_serialization)
+{
+    const auto a{card{.front = "Hello", .back = "Hallo"}};
+    auto ss{std::stringstream{}};
+    {
+        auto oa{::boost::archive::text_oarchive{ss}};
+        oa << a;
+    }
+    auto b{card{}};
+    {
+        auto ia{::boost::archive::text_iarchive{ss}};
+        ia >> b;
+    }
+    BOOST_TEST(a == b);
+}
+
+BOOST_AUTO_TEST_CASE(chapter_serialization)
+{
+    const auto a{chapter{.name = "ch_01",
+                         .cards = {{.front = "Hello", .back = "Hallo"}}}};
+    auto ss{std::stringstream{}};
+    {
+        auto oa{boost::archive::text_oarchive{ss}};
+        oa << a;
+    }
+    auto b{chapter{}};
+    {
+        auto ia{boost::archive::text_iarchive{ss}};
+        ia >> b;
+    }
+
+    BOOST_TEST(a == b);
+}
+
+BOOST_AUTO_TEST_CASE(book_serialization)
+{
+    const auto a{
+        book{.name = "pack_01",
+             .chapters = {{.name = "ch_01",
+                           .cards = {{.front = "Hello", .back = "Hallo"}}}}}};
+    auto ss{std::stringstream{}};
+    {
+        auto oa{boost::archive::text_oarchive{ss}};
+        oa << a;
+    }
+    auto b{book{}};
+    {
+        auto ia{boost::archive::text_iarchive{ss}};
+        ia >> b;
+    }
+    BOOST_TEST(a == b);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
+}
