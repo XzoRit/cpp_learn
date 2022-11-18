75f7108f1b8c8e4daf96ee8df603391c1086e4e1
XzoRit <xzorit@hotmail.com>
2022-11-18 19:25:48 +0100

    use console actions instead of data action for view changes

--- app/console.cpp
+++ app/console.cpp
@@ -12,9 +12,9 @@
 #include <string_view>
 #include <variant>
 
-using action = xzr::learn::data::action;
-using add_book = xzr::learn::data::add_book;
-using quit = xzr::learn::data::quit;
+using data_action = xzr::learn::data::action;
+using data_add_book = xzr::learn::data::add_book;
+using data_quit = xzr::learn::data::quit;
 using app = ::xzr::learn::data::app;
 using books = ::xzr::learn::data::books;
 
@@ -22,24 +22,36 @@ using ::xzr::learn::console::println;
 
 namespace
 {
+namespace console
+{
+struct add_book
+{
+};
+struct quit
+{
+};
+using action = std::variant<add_book, quit>;
+}
 auto readln() -> std::string
 {
     std::string str{};
     std::getline(std::cin, str);
     return str;
 }
-auto intent(std::string_view cmd) -> std::optional<action>
+auto intent(std::string_view cmd) -> std::optional<console::action>
 {
-    static const std::map<std::string_view, action> cmd_actions{
-        {{"a", add_book{}}, {"d", quit{}}}};
+    static const std::map<std::string_view, console::action> cmd_actions{
+        {{"a", console::add_book{}}, {"d", console::quit{}}}};
 
     if (const auto match{cmd_actions.find(cmd)}; match != cmd_actions.cend())
         return match->second;
     return std::nullopt;
 }
-auto update(app app_data, action act) -> app
+auto update(app app_data, console::action act) -> app
 {
-    std::visit(boost::hof::match([&](add_book) {}, [&](quit) {}), act);
+    std::visit(
+        boost::hof::match([&](console::add_book) {}, [&](console::quit) {}),
+        act);
 
     return app_data;
 }
@@ -73,7 +85,7 @@ auto run(app app_data) -> void
         {
             app_data = ::update(std::move(app_data), act.value());
             ::render(app_data);
-            if (std::holds_alternative<quit>(act.value()))
+            if (std::holds_alternative<::console::quit>(act.value()))
                 return;
         }
     }
