4506b45ce53d3923f4c802827c083c143bd03dea
XzoRit <xzorit@hotmail.com>
2022-11-18 19:53:40 +0100

    it shall be possible to add a new book

--- app/console.cpp
+++ app/console.cpp
@@ -3,6 +3,7 @@
 #include <data/action.hpp>
 #include <data/app.hpp>
 #include <data/data.hpp>
+#include <data/update.hpp>
 
 #include <boost/hof/match.hpp>
 
@@ -10,6 +11,7 @@
 #include <optional>
 #include <string>
 #include <string_view>
+#include <tuple>
 #include <variant>
 
 using data_action = xzr::learn::data::action;
@@ -38,22 +40,36 @@ auto readln() -> std::string
     std::getline(std::cin, str);
     return str;
 }
+auto show_add_book() -> data_add_book
+{
+    println("add book");
+    println();
+    println("name: ");
+
+    return data_add_book{.name = readln()};
+}
 auto intent(std::string_view cmd) -> std::optional<console::action>
 {
     static const std::map<std::string_view, console::action> cmd_actions{
-        {{"a", console::add_book{}}, {"d", console::quit{}}}};
+        {{"b", console::add_book{}}, {"d", console::quit{}}}};
 
     if (const auto match{cmd_actions.find(cmd)}; match != cmd_actions.cend())
         return match->second;
     return std::nullopt;
 }
-auto update(app app_data, console::action act) -> app
+auto update(app app_data, console::action act)
+    -> std::tuple<app, std::optional<data_action>>
 {
-    std::visit(
-        boost::hof::match([&](console::add_book) {}, [&](console::quit) {}),
+    return std::visit(
+        boost::hof::match(
+            [&](console::add_book)
+                -> std::tuple<app, std::optional<data_action>> {
+                return {app_data, show_add_book()};
+            },
+            [&](console::quit) -> std::tuple<app, std::optional<data_action>> {
+                return {app_data, std::nullopt};
+            }),
         act);
-
-    return app_data;
 }
 auto render_books(const books& bs) -> void
 {
@@ -77,15 +93,22 @@ inline namespace v1
 {
 auto run(app app_data) -> void
 {
-    ::render(app_data);
+    auto app_data_and_action{
+        std::make_tuple(app_data, std::optional<data_action>{})};
     for (;;)
     {
+        ::render(std::get<0>(app_data_and_action));
         const auto cmd{::readln()};
-        if (const auto act{::intent(cmd)})
+        if (const auto console_act{::intent(cmd)})
         {
-            app_data = ::update(std::move(app_data), act.value());
-            ::render(app_data);
-            if (std::holds_alternative<::console::quit>(act.value()))
+            app_data_and_action =
+                ::update(std::move(std::get<0>(app_data_and_action)),
+                         console_act.value());
+            if (const auto data_act{std::get<1>(app_data_and_action)})
+                std::get<0>(app_data_and_action) = ::xzr::learn::update::update(
+                    std::move(std::get<0>(app_data_and_action)),
+                    data_act.value());
+            if (std::holds_alternative<::console::quit>(console_act.value()))
                 return;
         }
     }
